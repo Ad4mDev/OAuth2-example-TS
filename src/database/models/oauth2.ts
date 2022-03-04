f4e2cccf4a1707280ad8e6170011f45e2e6d80f6
Ad4mDev <96986853+Ad4mDev@users.noreply.github.com>
2022-03-04 23:36:18 +0100

    Update oauth2.ts

--- src/database/models/oauth2.ts
+++ src/database/models/oauth2.ts
@@ -1,2 +1,14 @@
 import Mongoose from 'mongoose';
 import { schema, field, buildSchema } from 'mongoose-schema-decorators';
+
+@schema({ _id: false })
+class Oauth2 {
+  @field
+  public access_token: string;
+
+  @field
+  public refresh_token: string;
+
+  @field
+  public id: string;
+}
